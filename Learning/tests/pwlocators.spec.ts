b27595f4230ca1d5457e2be35038f11efeeaf288
Praveen <praveen.padmanaban@vvtsolutions.in>
2026-01-29 18:47:40 +0530

    third commit

--- Learning/tests/pwlocators.spec.ts
+++ Learning/tests/pwlocators.spec.ts
@@ -1,3 +1,29 @@
- import {test,expect} from '@playwright/test';
+ import {test,expect, Locator} from '@playwright/test';
+/*
+Locators - Identifies the element on the page 
+Dom- Document object model
+Dom is an API interface provided by browser
 
- test("verify login page ")
+1) Page.getByAltText() to locate an element, usually image, by its text alternative.by using alt tag
+2)page.getByText() to locate by text content
+
+
+
+*/
+test("verify image having the alttxt",async ({page})=>{
+//1.page.getByText()-identifies images (and similar elements ) based on the alt attribute.
+// use this locators when your element supports alt txt such as img area elements.
+await page.goto("https://www.flipkart.com");
+const logo:Locator =page.getByAltText("Minutes")
+await expect(logo).toBeVisible();
+await logo.click();
+
+});
+
+test("Verify the element by text in it ",async ({page})=>{
+
+await page.goto("https://www.flipkart.com");
+const text:Locator=page.getByText("Cart")
+await expect(text)
+await text.click();
+});
